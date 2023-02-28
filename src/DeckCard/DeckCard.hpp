d718a6a475d0dd8eea96482e670a735325db3243
GoDillonAudris512 <110383663+GoDillonAudris512@users.noreply.github.com>
2023-02-28 20:36:02 +0700

    feat: add class deck card header

---
+++ src/DeckCard/DeckCard.hpp
@@ -0,0 +1,45 @@
+// File: DeckCard.hpp
+// Berisi deklarasi dari kelas DeckCard
+
+#ifndef DECKCARD_HPP
+#define DECKCARD_HPP
+
+#include "../InventoryHolder/InventoryHolder.hpp"
+
+class DeckCard : public InventoryHolder {
+    public:
+        /*--------------------------------------------------------------------*/
+        /*------------------CREATION AND DESTRUCTION SEGMENT------------------*/
+        
+        // ctor
+        DeckCard();
+
+        // user-defined ctor: copy the listOfCard to this object listOfCard attribute
+        DeckCard(const vector<Card>& listOfCard);
+
+        // dtor
+        ~DeckCard() {};
+
+        // operator= 
+        DeckCard& operator=(const DeckCard& other);
+
+        /*--------------------------------------------------------------------*/
+        /*------------------------ADDING CARDS SEGMENT------------------------*/
+        
+        // Generate all 52 cards randomly
+        void addCards(const InventoryHolder& other);
+
+        // Return deck card with 1 more card (other)
+        DeckCard operator+(const Card& other);
+
+        /*--------------------------------------------------------------------*/
+        /*-----------------------REMOVING CARDS SEGMENT-----------------------*/
+
+        // Remove the first <cardsRemoved> cards from the deck
+        void removeCards(int cardsRemoved);
+
+        // Return deck card with <cardsRemoved> less card
+        DeckCard operator-(int cardsRemoved);
+};
+
+#endif
