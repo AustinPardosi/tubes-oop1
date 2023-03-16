e3d7ae7236f4e6d1e947a42198dfdc54435db300
Michael Leon Putra Widhi <leonmichael463@gmail.com>
2023-03-16 13:57:07 +0700

    PARSE NYA KENAPA ERROR
    PARSE NYA KENAPA ERROR

--- src/Parsing/Parse.cpp
+++ src/Parsing/Parse.cpp
@@ -8,7 +8,7 @@
 
 Parse::Parse() {
     this->totalLine = 0;
-    this->safe = true;
+    this->safe = false;
 }
 
 /*--------------------------------------------------------------------*/
@@ -36,55 +36,53 @@ void Parse::parsing(string inputfile) {
     int cardNumber, cardColor;
     ifstream inputFile(inputfile);
 
-    try {
-        // Read card from txt file
-        while (getline(inputFile, line)) {
-            this->totalLine++;
+    while (!this->safe) {
+        try {
+            // Read card from txt file
+            while (getline(inputFile, line)) {
+                this->totalLine++;
 
-            // Validate every line must have 2 values
-            istringstream iss(line);
-            if (!(iss >> cardNumber >> cardColor) || (iss >> cardColor)) {
-                throw new CardFileException(0);
-                this->safe = false;
+                // Validate every line must have 2 values
+                istringstream iss(line);
+                if (!(iss >> cardNumber >> cardColor) || (iss >> cardColor)) {
+                    // this->safe = false;
+                    throw new CardFileException(0);
+                }
+
+                // Validate value of cardNumber and cardColor
+                // 1 <= cardNumber <= 13 dan 0 <= cardColor <= 3
+                if (cardNumber < 1 || cardNumber > 13 || cardColor < 0 || cardColor > 3) {
+                    // this->safe = false;
+                    throw new CardFileException(1);
+                }
+
+                validateCards.push_back(make_pair(cardNumber, cardColor));
             }
 
-            // Validate value of cardNumber and cardColor
-            // 1 <= cardNumber <= 13 dan 0 <= cardColor <= 3
-            if (cardNumber < 1 || cardNumber > 13 || cardColor < 0 || cardColor > 3) {
-                throw new CardFileException(1);
-                this->safe = false;
+            // Validate totalLine must be 52
+            if (this->totalLine != 52) {
+                // this->safe = false;
+                throw new CardFileException(2);
             }
 
-            validateCards.push_back(make_pair(cardNumber, cardColor));
-        }
-
-        // Validate totalLine must be 52
-        if (this->totalLine != 52) {
-            throw new CardFileException(2);
-            this->safe = false;
-        }
-        
-        // Validate every card unique
-        if (this->safe) {
+            
             for(int i = 0; i < validateCards.size(); i++) {
                 for(int j = i + 1; j < validateCards.size(); j++) {
                     if(validateCards[i] == validateCards[j]) {
                         throw new CardFileException(3);
-                        this->safe = false;
                     }
                 }
             }
-        }
-    } catch (BaseException* e) {
-        e->printMessage();
-    }
-    
-    // Insert into cards
-    if (this->safe) {
-        for(const auto& i : validateCards) {
-            this->cards.emplace_back(i);
-        }
-    }
 
-    inputFile.close();
+            // Insert into cards
+            for(const auto& i : validateCards) {
+                this->cards.emplace_back(i);
+            }
+
+            this->safe = true;
+            inputFile.close();
+        } catch (BaseException* e) {
+            e->printMessage();
+        }
+    }
 }
